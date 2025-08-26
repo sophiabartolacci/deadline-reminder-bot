cc239d335a644702bde596c632eeb61a92e93990
Sophia Bartolacci <sbartolacci@resurety.com>
2025-08-26 14:59:53 -0400

    sample send message, store assignment info in a cleaner way

--- bot.py
+++ bot.py
@@ -6,10 +6,13 @@ from dotenv import load_dotenv
     command to run the bot.py file: source venv/bin/activate && python bot.py
 '''
 
-# Load environment variables
+# Setup
 load_dotenv('.env')
 notion = AsyncClient(auth=os.environ["NOTION_TOKEN"])
 database_id = os.environ["NOTION_DATABASE_ID"]
+
+intents = discord.Intents.default()
+discord_client = discord.Client(intents=intents)
 
 async def db_setup() -> dict:
     database = await notion.databases.retrieve(database_id)
@@ -20,13 +23,10 @@ def get_three_days_out() -> datetime:
     three_days_out = today + datetime.timedelta(days=3)
     return three_days_out
 
-async def get_assignments_due_date() -> None:
-    '''
-        This function retrieves all assignments that are not done and returns the assignments' due date
-    '''
+async def filter_db() -> dict:
     three_days_out = get_three_days_out().isoformat()
     
-    assignments = await notion.databases.query(
+    assignment_info = await notion.databases.query(
         database_id=database_id,
         filter={
             "and": [
@@ -51,22 +51,55 @@ async def get_assignments_due_date() -> None:
             ]
         }
     )
-    results = assignments["results"]
-    for assignment in results:
-        assignment_name = assignment['properties']['Assignment']['title'][0]['plain_text']
-        assignment_class = assignment['properties']['Class']['select']['name']
-        assignment_type = assignment['properties']['Type']['select']['name']
-        assignment_due_date = assignment['properties']['Due Date']['date']['start']
-        
-        print(f"Assignment: {assignment_name}")
-        print(f"Class: {assignment_class}")
-        print(f"Type: {assignment_type}")
-        print(f"Due Date: {assignment_due_date}")
-        print("-" * 30)
+    return assignment_info
 
-async def test_get_db_info() -> None:
-    properties = await db_setup()
-    print(properties)
+def extract_assignment_info() -> list:
+    assignment_info = asyncio.run(filter_db())
+    all_assignments = []
+    results = assignment_info["results"]
+    for entry in results:
+        assignment_name = entry['properties']['Assignment']['title'][0]['plain_text']
+        assignment_class = entry['properties']['Class']['select']['name']
+        assignment_type = entry['properties']['Type']['select']['name']
+        assignment_due_date = entry['properties']['Due Date']['date']['start']
+        notes_list = entry['properties']['Notes']['rich_text']
+        
+        # Check if notes_list is empty
+        if not notes_list:  
+            assignment_notes = "No notes"
+        else:
+            assignment_notes = notes_list[0]['plain_text']
+
+        # Store all assignment information in a dictionary
+        entry_info = {
+            "name": assignment_name,
+            "class": assignment_class,
+            "type": assignment_type,
+            "due_date": assignment_due_date,
+            "notes": assignment_notes
+        }
+
+        # Add each entry to a list of all assignments
+        all_assignments.append(entry_info)
+    print(all_assignments)
+    return all_assignments
+
+def send_message():
+    token = os.environ["DISCORD_TOKEN"]
+    channel_id = int(os.environ["DISCORD_CHANNEL_ID"])
+    
+    client = discord.Client(intents=discord.Intents.default())
+    
+    @client.event
+    async def on_ready():
+        channel = client.get_channel(channel_id)
+        await channel.send("Hello World!")
+        await client.close()
+    
+    client.run(token)
+
 
 if __name__ == "__main__":
-    asyncio.run(get_assignments_due_date())
+    # send_message()
+    # extract_assignment_info()
+    
